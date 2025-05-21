c62e52da06e4a18658485003f4a146c7dc083b75
purple8cloud <ljh4u2000@outlook.com>
2025-05-22 03:14:56 +0900

    파일 정리 및 1인칭 캐릭터 베이스 추가

--- Source/RhythmGunner/private/RhythmGunnerGameMode.cpp
+++ Source/RhythmGunner/private/RhythmGunnerGameMode.cpp
@@ -7,9 +7,9 @@
 ARhythmGunnerGameMode::ARhythmGunnerGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	/*static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/BluePrints/BP_RhythmGunnerCharacter.BP_RhythmGunnerCharacter_C"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}*/
+	}
 }
