779ecfb9fe0cd0171e3c575bfd646e2eeb612eb2
water0326 <bounce0326@naver.com>
2025-05-23 06:08:40 +0900

    re-created game manager in gamemode

--- Source/RhythmGunner/private/RhythmGunnerGameMode.cpp
+++ Source/RhythmGunner/private/RhythmGunnerGameMode.cpp
@@ -3,6 +3,9 @@
 #include "RhythmGunnerGameMode.h"
 #include "RhythmGunnerCharacter.h"
 #include "UObject/ConstructorHelpers.h"
+#include "GameFramework/PlayerController.h"
+#include "Kismet/GameplayStatics.h"
+#include "RhythmSubsystem.h"
 
 ARhythmGunnerGameMode::ARhythmGunnerGameMode()
 {
@@ -11,5 +14,69 @@ ARhythmGunnerGameMode::ARhythmGunnerGameMode()
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
+
+void ARhythmGunnerGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+	SetupInputBindings();
+	DisplayShortcutInfo();
+}
+
+void ARhythmGunnerGameMode::SetupInputBindings()
+{
+	if (APlayerController* PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0))
+	{
+		// Ctrl + 1: 일시정지/재개
+		PlayerController->InputComponent->BindKey(FKey("One"), EInputEvent::IE_Pressed, this, &ARhythmGunnerGameMode::TogglePause).bExecuteWhenPaused = true;
+		// Ctrl + 2: 레벨 재시작
+		//PlayerController->InputComponent->BindKey(FKey("Two"), EInputEvent::IE_Pressed, this, &ARhythmGunnerGameMode::RestartLevel).bExecuteWhenPaused = true;
+	}
+}
+
+void ARhythmGunnerGameMode::TogglePause()
+{
+	if (APlayerController* PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0))
+	{
+		if (PlayerController->IsPaused())
+		{
+			ResumeGame();
+		}
+		else
+		{
+			PauseGame();
+		}
+	}
+}
+
+void ARhythmGunnerGameMode::PauseGame()
+{
+	if (APlayerController* PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0))
+	{
+		PlayerController->SetPause(true);
+	}
+}
+
+void ARhythmGunnerGameMode::ResumeGame()
+{
+	if (APlayerController* PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0))
+	{
+		PlayerController->SetPause(false);
+	}
+}
+
+void ARhythmGunnerGameMode::RestartLevel()
+{
+	
+}
+
+void ARhythmGunnerGameMode::DisplayShortcutInfo()
+{
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, TEXT("단축키 안내:"));
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::White, TEXT("Ctrl + 1: 게임 일시정지/재개"));
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::White, TEXT("Ctrl + 2: 레벨 재시작 (현재 비활성화)"));
 	}
 }
--- Source/RhythmGunner/public/RhythmGunnerGameMode.h
+++ Source/RhythmGunner/public/RhythmGunnerGameMode.h
@@ -13,6 +13,30 @@ class ARhythmGunnerGameMode : public AGameModeBase
 
 public:
 	ARhythmGunnerGameMode();
+
+	// 게임 일시정지
+	UFUNCTION(BlueprintCallable, Category = "Game")
+	void PauseGame();
+
+	// 게임 재개
+	UFUNCTION(BlueprintCallable, Category = "Game")
+	void ResumeGame();
+
+	// 현재 레벨 재시작
+	UFUNCTION(BlueprintCallable, Category = "Game")
+	void RestartLevel();
+
+protected:
+	virtual void BeginPlay() override;
+
+	// 입력 바인딩 설정
+	void SetupInputBindings();
+
+	// 일시정지 토글
+	void TogglePause();
+
+	// 단축키 정보 표시
+	void DisplayShortcutInfo();
 };
 
 
